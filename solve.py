ac45abb408bd4d96300e459201618bfb99c2e7c2
Evan <aaagentyugituber@gmail.com>
2021-01-05 20:49:15 -0500

    fully implement backtracking algorithm

--- solve.py
+++ solve.py
@@ -1,45 +1,81 @@
-# define the original board
-board = [
-    [7,8,0,4,0,0,1,2,0],
-    [6,0,0,0,7,5,0,0,9],
-    [0,0,0,6,0,1,0,7,8],
-    [0,0,7,0,4,0,2,6,0],
-    [0,0,1,0,5,0,9,3,0],
-    [9,0,4,0,6,0,0,0,5],
-    [0,7,0,3,0,0,0,1,2],
-    [1,2,0,0,0,7,4,0,0],
-    [0,4,9,2,0,6,0,0,7]
-]
+from constants import NUM_ROWS, NUM_COLUMNS, TEST_BOARD
 
-def valid_add(board, num, position):
-    for c in range(len(board[0])):
-        if board[position[0]][c] == num and position[1] != c:
-            return False
+# assign a TEST_BOARD to board variable
+# is a list of length 9 of lists of integers 0 through 9 of length 9
+board = TEST_BOARD
 
-
-def print_board(board):
-    for r in range(len(board)):
-        if r%3 == 0 and r != 0:
-            print("- - - - - - - - - - - - - - - -")
-
-        for c in range(len(board[0])):
-            if c%3 == 0 and c != 0:
-                print("| ", end="")
-            
-            if c == 8:
-                print(board[r][c])
-            else:
-                print(board[r][c], " ", end="")
-
-
+# find an empty square on the board
+# returns a tuple of (row, column) or None
 def empty_square(board):
-    for r in range(len(board)):
-        for c in range(len(board[0])):
+    for r in range(NUM_ROWS):
+        for c in range(NUM_COLUMNS):
             if board[r][c] == 0:
                 return (r, c)
     return None
 
-print_board(board)
-#solve(board)
+
+
+# check if adding insert to the position onto the board is a valid move
+# returns True if valid, False if not
+def valid_add(board, insert, position):
+    # check row
+    for c in range(NUM_COLUMNS):
+        if board[position[0]][c] == insert and position[1] != c:
+            return False
+    
+    # check column
+    for r in range(NUM_ROWS):
+        if board[r][position[1]] == insert and position[0] != r:
+            return False
+    
+    # check box
+    # boxes labelled as such
+    # (0,0) | (0, 1) | (0,2)
+    # ++++++++++++++++++++++
+    # (1,0) | (1, 1) | (1,2)
+    # ++++++++++++++++++++++
+    # (2,0) | (2, 1) | (2,2)
+    box_x = position[1]//3 
+    box_y = position[0]//3
+    for r in range(box_y*3, box_y*3+3):
+        for c in range(box_x*3, box_x*3+3):
+            if (r,c) != position and board[r][c] == insert:
+                return False
+    
+    return True
+
+
+
+def solve_board(board):
+    empty = empty_square(board)
+    if not empty: # no more empty board spaces means that the board is solved under the backtracking algorithm
+        return True
+    else:
+        for number in range(1,10):
+            if valid_add(board, number, empty):
+                board[empty[0]][empty[1]] = number
+
+                if solve_board(board): # recursively call solve_board and if no solution exists by adding the current number
+                    return True
+                
+                board[empty[0]][empty[1]] = 0 # then remove this number from the position
+
+
+
+def print_board(board):
+    for r in range(NUM_ROWS):
+        if r%3 == 0 and r != 0:
+            print("+ + + + + + + + + + + + + + +")
+
+        for c in range(NUM_COLUMNS):
+            if c%3 == 0 and c != 0:
+                print("| ", end="")
+            elif c == 8:
+                print(board[r][c])
+            else:
+                print(board[r][c], " ", end="")
+    print() # to print a newline at the end of a printing a board
+
 #print_board(board)
-        
+#solve_board(board)
+#print_board(board)
