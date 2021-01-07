ad610fad5de82a36ff7810512b04b2207fef457b
Evan Luo <luohevan@gmail.com>
2021-01-07 15:15:23 -0500

    input numbers and check for bad squares

--- game.py
+++ game.py
@@ -9,20 +9,29 @@ class Game:
         pygame.init()
         pygame.display.set_caption("Sudoku")
         self.window = pygame.display.set_mode((WIDTH, HEIGHT))
-        self.running = True
+        self.running = False
         self.board = TEST_BOARD
+        self.solution = TEST_BOARD_COPY
+        solve_board(self.solution)
+        self.solved = False
+        self.original_filled = []
+        self.sketch_filled = []
+        self.entered_filled = []
         self.selected_square = None
         self.mouse_position = None
-        self.font = pygame.font.SysFont("arial", CELL_SIZE//2)
+        self.font = pygame.font.SysFont("comicsans", 40)
         self.play_time = True
         self.play_buttons = []
         self.menu_buttons = []
         self.end_buttons = []
-        self.load_buttons()
+        self.highlight_incorrect_mode = True
+        self.incorrect_tracker = False
+        self.incorrect_submissions = 0
         
-    def start_game(self):
+    def start_game(self):                
+        self.startup()
         #self.start_time = pygame.time.get_ticks()
-
+        self.running = True
         while self.running:
             if self.play_time:
                 self.handle_events()
@@ -38,10 +47,10 @@ class Game:
                 self.running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 self.selected_square = self.board_position()
-                if not self.selected_square:
+                if not self.selected_square or self.selected_square in self.original_filled:
                     self.selected_square = None
 
-            #if event.type == pygame.KEYDOWN:
+            if event.type == pygame.KEYDOWN:
             #    if event.key == pygame.K_LEFT:
                     # move to grid 
             #    elif event.key == pygame.K_RIGHT:
@@ -51,24 +60,15 @@ class Game:
             #    elif event.key == pygame.K_DOWN:
                     #move to grid
                 
-            #    if event.key == pygame.K_1:
-            #        typed = 1
-            #    elif event.key == pygame.K_2:
-            #        typed = 2
-            #    elif event.key == pygame.K_3:
-            #        typed = 3
-            #    elif event.key == pygame.K_4:
-            #        typed = 4
-            #    elif event.key == pygame.K_5:
-            #        typed = 5
-            #    elif event.key == pygame.K_6:
-            #        typed = 6
-            #    elif event.key == pygame.K_7:
-            #        typed = 7
-            #    elif event.key == pygame.K_8:
-            #        typed = 8
-            #    elif event.key == pygame.K_9:
-            #        typed = 9
+                if self.selected_square != None:
+                    try:
+                        int(event.unicode)
+                        self.board[self.selected_square[1]][self.selected_square[0]] = int(event.unicode)
+                        self.sketch_filled.append(self.selected_square)
+                        self.cell_edited = True
+
+                    except:
+                        pass
             #    elif event.key == pygame.K_BACKSPACE or event.key == pygame.K_RETURN:
             #        typed = None 
 
@@ -84,6 +84,16 @@ class Game:
         for button in self.play_buttons:
             button.update_button_state(self.mouse_position)
 
+        if self.highlight_incorrect_mode:
+            self.incorrect_submissions = []
+            self.check_for_incorrect()
+       
+    def check_for_incorrect(self):
+        for r in range(NUM_ROWS):
+            for c in range(NUM_COLUMNS):
+                if self.board[r][c] != 0 and self.board[r][c] != self.solution[r][c]:
+                    self.incorrect_submissions.append((c, r))
+
     def draw(self):
         self.window.fill(WHITE)
 
@@ -92,6 +102,9 @@ class Game:
 
         if self.selected_square:
             self.draw_selected(self.window, self.selected_square)
+        
+        if len(self.incorrect_submissions):
+            self.draw_incorrect(self.window)
 
         self.draw_board(self.window)
         self.draw_board_numbers(self.window)
@@ -109,14 +122,31 @@ class Game:
 
 
     def draw_board_numbers(self, window):
-        pass
+        for r in range(NUM_ROWS):
+            for c in range(NUM_COLUMNS):
+                if self.board[r][c] != 0:
+                    pos = (c*CELL_SIZE+BOARD_POSITION[0], r*CELL_SIZE+BOARD_POSITION[1])
+                    self.draw_number(window, str(self.board[r][c]), pos)
 
     def draw_selected(self, window, position):
         pygame.draw.rect(window, LIGHT_BLUE, (position[0]*CELL_SIZE+BOARD_POSITION[0], position[1]*CELL_SIZE+BOARD_POSITION[1], CELL_SIZE, CELL_SIZE))
 
+    
+    def draw_incorrect(self, window):
+        for position in self.incorrect_submissions:
+            pygame.draw.rect(window, INCORRECT_RED, (position[0]*CELL_SIZE+BOARD_POSITION[0], position[1]*CELL_SIZE+BOARD_POSITION[1], CELL_SIZE, CELL_SIZE))
+    
     def load_buttons(self):
         self.play_buttons.append(Button(20,40,100,40))
 
-    def draw_text(self, window, text, position):
-        font = self.font.render(text, False, BLACK)
-        window.blit(font, position)
+    def draw_number(self, window, number, position):
+        printed_number = self.font.render(number, False, BLACK)
+        adjusted_position = (position[0] + (CELL_SIZE - printed_number.get_width())//2, position[1] + (CELL_SIZE - printed_number.get_height())//2)
+        window.blit(printed_number, adjusted_position)
+
+    def startup(self):
+        for r in range(NUM_ROWS):
+            for c in range(NUM_COLUMNS):
+                if self.board[r][c] != 0:
+                    self.original_filled.append((c, r))
+        self.load_buttons()
