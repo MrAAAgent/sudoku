d7356ca4d08cbe31166fc9f38abc46a59bbda84d
Evan Luo <luohevan@gmail.com>
2021-01-08 14:30:09 -0500

    load test board if webscrape fails

--- puzzle.py
+++ puzzle.py
@@ -1,25 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
-from constants import NUM_ROWS, NUM_COLUMNS
+from constants import NUM_ROWS, NUM_COLUMNS, TEST_BOARD
 from solve import solve_board
 
 def get_puzzle(difficulty):
-    websudoku_html = requests.get("https://nine.websudoku.com/?level={}".format(difficulty)).content
-    soup = BeautifulSoup(websudoku_html, 'html.parser')
-    puzzle_ids = []
-    for r in range(NUM_ROWS):
-        for c in range(NUM_COLUMNS):
-            puzzle_ids.append("f{}{}".format(c,r))
-    data = []
-    for id in puzzle_ids:
-        data.append(soup.find('input', id=id))
-    board = [[0 for x in range(NUM_COLUMNS)] for x in range(NUM_ROWS)]
-    for index, cell in enumerate(data):
-        try:
-           board[index//9][index%9] = int(cell['value'])
-        except:
-            pass
-    return board
+    try:
+        websudoku_html = requests.get("https://nine.websudoku.com/?level={}".format(difficulty)).content
+        soup = BeautifulSoup(websudoku_html, 'html.parser')
+        puzzle_ids = []
+        for r in range(NUM_ROWS):
+            for c in range(NUM_COLUMNS):
+                puzzle_ids.append("f{}{}".format(c,r))
+        data = []
+        for id in puzzle_ids:
+            data.append(soup.find('input', id=id))
+        board = [[0 for x in range(NUM_COLUMNS)] for x in range(NUM_ROWS)]
+        for index, cell in enumerate(data):
+            board[index//9][index%9] = int(cell['value'])
+        return board
+    except:
+        return TEST_BOARD
+
 
 def get_solution(board):
     solution = [[0 for x in range(NUM_COLUMNS)] for x in range(NUM_ROWS)]
