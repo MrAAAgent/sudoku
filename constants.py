fd907f8878168b4e8518a3d44326067915c0ef0d
Evan Luo <luohevan@gmail.com>
2021-01-08 14:29:23 -0500

    add constants

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 NUM_ROWS = 9
 NUM_COLUMNS = 9
 WIDTH = 600
-HEIGHT = 600
+HEIGHT = 650
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -22,7 +22,7 @@ TEST_BOARD = [
     [2,0,0,5,0,3,0,0,0]
 ]
 
-BOARD_POSITION = (75,100)
+BOARD_POSITION = (75,125)
 CELL_SIZE = 50
 BOARD_SIZE = CELL_SIZE*9
 
@@ -34,3 +34,14 @@ EVIL = "4"
 #ORIGINAL_FILLED_FONT
 #SKETCH_FONT
 #LOCKED_FONT
+
+WINDOW_TITLE = "Sudoku - "
+EASY_MODE_TITLE = "Easy"
+MEDIUM_MODE_TITLE = "Medium"
+HARD_MODE_TITLE = "Hard"
+EVIL_MODE_TITLE = "Evil"
+HIGHLIGHT_ERROR_TITLE = " (Training Mode)"
+TRACK_ERROR_TITLE = " (Survival Mode)"
+
+START_NEW_GAME_TITLE = "Start New Game"
+START_NEW_GAME_MESSAGE = "You will lose all your progress on your current game. Are you sure you want to start a new game?"
